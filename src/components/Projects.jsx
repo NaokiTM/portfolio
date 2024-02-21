7f6e57e284deceefeb4712e263b3c9dce18ec9e2
naoki miyagawa <ntsome21@gmail.com>
2024-02-21 12:17:24 +0000

    added animated RGB text to project component heading

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='text-white bg-black relative h-screen'>
-      <div className='animate-rgbtext'>
+      <div className='text'>
         <div className="text-2xl font-mono flex p-6">Projects:</div>
       </div>
     </div>
